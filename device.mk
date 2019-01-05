0fd7c05d0e8120a6b127653b7dc832075796f7c0
Albert I <krascgq@outlook.co.id>
2019-01-05 19:49:23 +0800

    tiffany: Make tree compatible with both 3.18 and 4.9 kernels
    * Restored 3.18's legacy /devices/soc path in both fstab variants.

--- device.mk
+++ device.mk
@@ -343,7 +343,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.goodix.sh \
-    init.msm.usb.configfs.rc \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.rc \
@@ -354,6 +353,11 @@ PRODUCT_PACKAGES += \
     init.target.rc \
     init.tiffany.rc \
     ueventd.qcom.rc
+
+ifeq ($(TARGET_KERNEL_VERSION),4.9)
+PRODUCT_PACKAGES += \
+    init.msm.usb.configfs.rc
+endif
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
