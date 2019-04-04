d16533cf6e427f8d8dd291a470884650b8f3802f
Artem Borisov <dedsa2002@gmail.com>
2019-04-04 19:24:16 +0530

    tiffany: Build with BOARD_VNDK_VERSION=current
    Change-Id: I08a1bcf69bdf4ec2eaa93d727b50840778f99989

--- device.mk
+++ device.mk
@@ -56,3 +56,8 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
+
+# VNDK
+PRODUCT_PACKAGES += \
+    vndk_package \
+    libstdc++.vendor
