f6a49689e2ad927763b7c2953c80765ddf1d5e46
Artem Borisov <dedsa2002@gmail.com>
2019-04-11 17:26:45 +0530

    tiffany: Add dalvik property overrides
    * Yes, we already handle this in our init extension, but
      it can't be used with treble.
    
    Change-Id: Ibb31ae9fe5c8d547cd12f329cb694af463eace55

--- prop.mk
+++ prop.mk
@@ -11,3 +11,12 @@ vendor.camera.aux.packageblacklist=com.google.android.GoogleCamera,com.discord \
 vendor.camera.hal1.packagelist=com.whatsapp \
 persist.camera.dual.camera=0 \
 persist.camera.eis.enable=1
+
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+dalvik.vm.heapstartsize=16m \
+dalvik.vm.heapgrowthlimit=256m \
+dalvik.vm.heapsize=512m \
+dalvik.vm.heaptargetutilization=0.75 \
+dalvik.vm.heapminfree=4m \
+dalvik.vm.heapmaxfree=8m \
