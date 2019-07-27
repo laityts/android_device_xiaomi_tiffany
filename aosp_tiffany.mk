b363f0a0c64d6852ee1a6574c088c97b282797f8
laityts <laityts@gmail.com>
2019-07-27 12:09:44 +0800

    tiffany: update fingerprint from V10.3.1.0.ODBCNXM

--- aosp_tiffany.mk
+++ aosp_tiffany.mk
@@ -40,7 +40,7 @@ TARGET_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="tiffany-user 8.1.0 OPM1.171019.019 V10.2.3.0.ODBCNXM release-keys"
+    PRIVATE_BUILD_DESC="tiffany-user 8.1.0 OPM1.171019.019 V10.3.1.0.ODBCNXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "xiaomi/tiffany/tiffany:8.1.0/OPM1.171019.019/V10.2.3.0.ODBCNXM:user/release-keys"
+BUILD_FINGERPRINT := "xiaomi/tiffany/tiffany:8.1.0/OPM1.171019.019/V10.3.1.0.ODBCNXM:user/release-keys"
