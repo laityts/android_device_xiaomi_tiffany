bbc27cbc8e753f5c8a1818f98da5285ec358c8cf
jomadeto <jomadeto@gmail.com>
2020-11-07 08:09:58 +0100

    tiffany: Add LineageTouch again

--- device.mk
+++ device.mk
@@ -390,6 +390,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl:64 \
     android.hardware.thermal@1.0-service
 
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.xiaomi_8953
+
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
