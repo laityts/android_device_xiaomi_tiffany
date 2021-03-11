2e3e2a537b9ebf373feb711e78821fd929748b0e
laityts <laityts@gmail.com>
2021-03-11 18:03:26 +0800

    tiffany: fix fingerprint

--- device.mk
+++ device.mk
@@ -190,12 +190,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service-lazy \
     android.hardware.drm@1.3-service.clearkey
 
-# Fingerprint
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_mido \
-    fakelogprint \
-    libshims_gxfpd
-
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
